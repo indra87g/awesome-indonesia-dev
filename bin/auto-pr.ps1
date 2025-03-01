7d5ed122ca1641d680e1e2236a10cc3aeec0ffe2
Indra Sah Noeldy <twinightwheel50@gmail.com>
2025-03-01 14:33:32 +0700

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "indra87g/awesome-indonesia-dev:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
